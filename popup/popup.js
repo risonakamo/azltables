fae242078f6a0174c0143d444be74b84f63dfdbe
risonakamo <hmykanae@gmail.com>
2018-01-05 14:55:54 -0500

    crude buttons for popup so we can test out saving and fleet page

--- popup/popup.js
+++ popup/popup.js
@@ -2,14 +2,28 @@ window.onload=main;
 
 function main()
 {
+    initButtons();
     chrome.tabs.query({active:true,currentWindow:true},(tab)=>{
         if (tab[0].url.slice(0,28)=="https://azurlane.koumakan.jp")
         {
             chrome.tabs.executeScript({file:"azlinfohook.js"},(res)=>{
-                // document.body.innerHTML=JSON.stringify(res);
-
-                document.body.innerHTML=genShipTable(res[0]);
+                document.querySelector(".current-ship").innerHTML=genShipTable(res[0]);
             });
         }
+    });
+}
+
+function initButtons()
+{
+    var buttons=document.querySelectorAll(".button");
+
+    buttons[0].addEventListener("click",(e)=>{
+        chrome.tabs.create({
+            url:"fleettables/fleettables.html"
+        });
+    });
+
+    buttons[1].addEventListener("click",(e)=>{
+
     });
 }
