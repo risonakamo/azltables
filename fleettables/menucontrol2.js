7e11fcc8b48252af350417eef60a842316645b32
risonakamo <hmykanae@gmail.com>
2018-07-14 17:36:26 -0400

    rename things render fleetlists

--- fleettables/menucontrol2.js
+++ fleettables/menucontrol2.js
@@ -2,10 +2,21 @@
 //fleets: user fleets
 class MenuControl extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+
+        console.log(this.props.fleets);
+    }
+
     render()
     {
         return [
             React.createElement("div",{className:"fleet-list",key:"menutop1"},
+                this.props.fleets.map((x,i)=>{
+                    return React.createElement(FleetList,{fleet:x,key:`fleet${i}`});
+                }),
+
                 React.createElement("div",{className:"fleet-entry",key:"fleetlistall"},
                     React.createElement("div",{className:"inline-contain"},
                         React.createElement("div",{className:"overflow-contain all-filter"},
@@ -44,5 +55,41 @@ class MenuControl extends React.Component
                 )
             )
         ];
+    }
+}
+
+//FleetList(object fleet)
+//fleet: single fleet object
+class FleetList extends React.Component
+{
+    render()
+    {
+        return React.createElement("div",{className:"fleet-entry"},
+            React.createElement("div",{className:"inline-contain"},
+                React.createElement("div",{className:"overflow-contain"},
+                    (()=>{
+                        var res=[];
+
+                        for (var x=0,l=_shipClasses.length;x<l;x++)
+                        {
+                            if (!this.props.fleet.classes[_shipClasses[x]])
+                            {
+                                continue;
+                            }
+
+                            for (var y=0,ly=this.props.fleet.classes[_shipClasses[x]];y<ly;y++)
+                            {
+                                res.push(React.createElement("img",{className:_shipClasses[x],
+                                    src:`/shiptable/class/${_shipClasses[x]}.png`,key:`${x},${y}`}));
+                            }
+                        }
+
+                        return res;
+                    })()
+                ),
+
+                React.createElement("span",{className:"label"},this.props.fleet.name)
+            )
+        );
     }
 }
